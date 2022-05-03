97ed9eaf2e15783b38c492e0bf87de8773718a37
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-03 19:18:29 +0300

    add useEffect hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import Pet from './Pet.jsx'
 
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 
@@ -6,7 +8,21 @@ const SearchParams = () => {
   const [location, setLocation] = useState('')
   const [animal, setAnimal] = useState('')
   const [breed, setBreed] = useState('')
-  const breeds = ['poole', 'bichon']
+  const breeds = []
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    requestPets()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    )
+    const json = await res.json()
+
+    setPets(json.pets)
+  }
 
   return (
     <div className='search-params'>
@@ -68,6 +84,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   )
 }
