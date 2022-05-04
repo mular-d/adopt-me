97b6d0fef459476403925a91b903ce0c46d6ad67
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-04 23:23:51 +0300

    add class component

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,47 @@
+import { Component } from 'react'
 import { useParams } from 'react-router-dom'
 
-const Details = () => {
-  const { id } = useParams()
+class Details extends Component {
+  constructor(props) {
+    super(props)
 
-  return <div>{id}</div>
+    this.state = { loading: true }
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    )
+    const json = await res.json()
+
+    this.setState({ loading: false, ...json.pets[0] })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading</h2>
+    }
+
+    const { animal, breed, city, state, description, name } = this.state
+
+    return (
+      <div className='details'>
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city} - {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Details
+const WrapedDetails = () => {
+  const params = useParams()
+  return <Details params={params} />
+}
+
+export default WrapedDetails
