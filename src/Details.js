9137e25637537beed762122399a3fc23946b0858
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-05 12:48:08 +0300

    add modal using createPortal

--- src/Details.js
+++ src/Details.js
@@ -4,9 +4,10 @@ import { useParams } from 'react-router-dom'
 import Carousel from './Carousel.js'
 import ThemeContext from './ThemeContext'
 import ErrorBoundary from './ErrorBoundary'
+import Modal from './Modal'
 
 class Details extends Component {
-  state = { loading: true, redirect: false }
+  state = { loading: true, showModal: false }
 
   async componentDidMount() {
     const res = await fetch(
@@ -17,12 +18,15 @@ class Details extends Component {
     this.setState({ loading: false, ...json.pets[0] })
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal })
+
   render() {
     if (this.state.loading) {
       return <h2>loading</h2>
     }
 
-    const { animal, breed, city, state, description, name, images } = this.state
+    const { animal, breed, city, state, description, name, images, showModal } =
+      this.state
 
     return (
       <div className='details'>
@@ -34,10 +38,26 @@ class Details extends Component {
           </h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className='buttons'>
+                  <a href='https://bit.ly/pet-adopt'>Yes</a>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     )
