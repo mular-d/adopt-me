b47c5546484cc18b6fcda5d67d5a4bbf6a5d365d
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-05 08:36:43 +0300

    managing state in class component

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,14 @@
 import { Component } from 'react'
 import { useParams } from 'react-router-dom'
 
-class Details extends Component {
-  constructor(props) {
-    super(props)
+import Carousel from './Carousel.js'
 
-    this.state = { loading: true }
-  }
+class Details extends Component {
+  state = { loading: true }
 
   async componentDidMount() {
     const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+      `https://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     )
     const json = await res.json()
 
@@ -22,10 +20,11 @@ class Details extends Component {
       return <h2>loading</h2>
     }
 
-    const { animal, breed, city, state, description, name } = this.state
+    const { animal, breed, city, state, description, name, images } = this.state
 
     return (
       <div className='details'>
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
