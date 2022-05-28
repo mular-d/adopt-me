519c4e4b301b29809c2fdee9434048054a7fa08c
Muluneh Derese <muluneh.derese@protonmail.com>
2022-05-28 16:07:18 +0300

    fix styling

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,25 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = 'https://pet-images.dev-apis.com/pets/none.jpg'
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
-    hero = images[0]
+    hero = images[0];
   }
-  return (
-    <div>
-      <Link to={`/details/${id}`} className='pet'>
-        <div className='image-container'>
-          <img src={hero} alt={name} />
-        </div>
-        <div className='info'>
-          <h1>{name}</h1>
-          <h2>
-            {animal} - {breed} - {location}
-          </h2>
-        </div>
-      </Link>
-    </div>
-  )
-}
 
-export default Pet
+  return (
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </Link>
+  );
+};
+
+export default Pet;
